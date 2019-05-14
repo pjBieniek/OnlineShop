583e693b4ac154be4cf376f571616d140b5ac0e8
Pawel <p.j.bieniek@wp.pl>
2019-05-14 20:06:08 +0200

    sql database test

--- src/main/java/com/codecool/models/OnlineShop.java
+++ src/main/java/com/codecool/models/OnlineShop.java
@@ -1,8 +1,22 @@
 package com.codecool.models;
+import java.sql.*;
 
 public class OnlineShop {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         ShopRunner runner = new ShopRunner();
         runner.run();
+
+        Connection c = null;
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:test.db");
+            System.out.println("Sqlite DB connected");
+        } catch (Exception e){
+            System.out.println("\n...\n");
+            System.out.println(e);
+        }
+
+
     }
 }
