5890301b99bef54df478830df35f08c6067fd2db
Pawel <p.j.bieniek@wp.pl>
2019-05-20 13:01:37 +0200

    Add methods 'display' for String, List and Map input

--- src/main/java/com/codecool/View/Viewer.java
+++ src/main/java/com/codecool/View/Viewer.java
@@ -1,4 +1,24 @@
 package com.codecool.View;
 
+import com.codecool.models.Product;
+
+import java.util.List;
+import java.util.Map;
+
 public class Viewer {
+    public void display(String content){
+        System.out.println(content);
+    }
+
+    public void display(List<String> content){
+        for (Object element : content){
+            System.out.println(element);
+        }
+    }
+
+    public void display(Map<Product, Integer> content){
+        for (Map.Entry<Product, Integer> entry : content.entrySet()) {
+            System.out.println("Amount of: " + entry.getKey() + "is " + entry.getValue());
+        }
+    }
 }
