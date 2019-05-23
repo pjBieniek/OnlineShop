b8e09702ca50951d614e98af81703fe0abe77d1b
zed <mierwew@gmail.com>
2019-05-23 11:51:05 +0200

    shoprunner class implemented, started work on customer and customerController clases

--- src/main/java/com/codecool/models/OnlineShop.java
+++ src/main/java/com/codecool/models/OnlineShop.java
@@ -6,11 +6,7 @@ public class OnlineShop {
     public static void main(String[] args) throws Exception {
         ShopRunner runner = new ShopRunner();
         runner.run();
-        ProductDaoSQL shop = new ProductDaoSQL();
-//        shop.getAllProducts
-        shop.getAllProducts();
-        shop.updateProduct();
-        shop.getAllProducts();
+//
 
     }
 }
